7e5508a6e38da27028a7c681de68625cfa6a027b
VitaliyKashtanov <djvivi@tut.by>
2017-11-13 22:31:37 +0300

    edited version

--- main.js
+++ main.js
@@ -1,32 +1,24 @@
-function task() {
+var taskCall = document.getElementById('task');
+taskCall.onclick = function(){
+   addTask();
+}
+
+function addTask() {
 	var taskItem = document.createElement("div"),
 		taskBody = document.getElementById("task-body"),
 		taskItemP = document.createElement("p"),
 		button = document.createElement("button");
 		button.innerHTML = 'Delete';
 		button.className = 'btn-del';
-	
-	button.onclick = function() {
-		taskItem.remove();
-	} 
 
 	var task = document.querySelector("input").value;
 
 	if(task != "") {
 		var taskText = document.createTextNode(task);
-		var strikedText=false;
+		var strikedText = false;
 	} else {
 		alert('Please write your task');
 	}
-
-	taskItemP.onclick = function() {
-		if(strikedText) { 
-			taskItemP.style.textDecoration = ""; 
-		} else { 
-		taskItemP.style.textDecoration = "line-through"; 
-		} 
-		strikedText = !strikedText;
-	}  
 
 	taskItemP.appendChild(taskText);
 	taskItem.appendChild(taskItemP);
@@ -34,4 +26,26 @@ function task() {
 	taskBody.appendChild(taskItem);
 
 	document.querySelector("input").value = "";
+	// task = "";
+
+	taskItemP.onclick = function() {
+		strikeText();
+	}
+
+	function strikeText() {
+		if(strikedText) { 
+			taskItemP.style.textDecoration = ""; 
+		} else { 
+		taskItemP.style.textDecoration = "line-through"; 
+		} 
+		strikedText = !strikedText;
+	}
+
+	button.onclick = function() {
+		removeTask();
+	}
+	
+	function removeTask() {
+		taskItem.remove();
+	}
 }
